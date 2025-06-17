14275e66c581f114451ebe24fc52518a8740c15a
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2025-06-17 22:28:44 +0200

    added other imgs and aniamtion

---
+++ script.js
@@ -0,0 +1,22 @@
+  function openFullscreen(img) 
+  {
+    if (img.requestFullscreen) {
+      img.requestFullscreen();
+    } else if (img.webkitRequestFullscreen) { /* Safari */
+      img.webkitRequestFullscreen();
+    } else if (img.msRequestFullscreen) { /* IE11 */
+      img.msRequestFullscreen();
+    }
+  }
+
+  function openFullscreen(img) {
+  const overlay = document.getElementById('fullscreen-overlay');
+  const overlayImg = overlay.querySelector('img');
+  overlayImg.src = img.src;
+  overlay.classList.add('active');
+}
+
+function closeFullscreen() {
+  const overlay = document.getElementById('fullscreen-overlay');
+  overlay.classList.remove('active');
+}
