1abca11b91b005bb5d28ea9530db2cfc750e9dc8
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2025-06-18 01:20:58 +0200

    added anaimtions all over the place

---
+++ script2.js
@@ -0,0 +1,15 @@
+  const links = document.querySelectorAll("a");
+  const fade = document.querySelector(".fade-out");
+
+  links.forEach(link => {
+    // Make sure it's an internal link:
+    if (link.hostname === window.location.hostname) {
+      link.addEventListener("click", e => {
+        e.preventDefault();
+        fade.classList.add("active");
+        setTimeout(() => {
+          window.location = link.href;
+        }, 500); // must match CSS transition time
+      });
+    }
+  });
