93a2901117a2b8edd6db10923e13412c3aac5f85
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2025-06-18 23:50:49 +0200

    added a comment

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-  function openFullscreen(img) 
+  function openFullscreen(img) //to allow user to go full screen
   {
     if (img.requestFullscreen) {
       img.requestFullscreen();
-    } else if (img.webkitRequestFullscreen) { /* Safari */
+    } else if (img.webkitRequestFullscreen) {
       img.webkitRequestFullscreen();
-    } else if (img.msRequestFullscreen) { /* IE11 */
+    } else if (img.msRequestFullscreen) { 
       img.msRequestFullscreen();
     }
   }
 
-  function openFullscreen(img) {
+  function openFullscreen(img) 
+  {
   const overlay = document.getElementById('fullscreen-overlay');
   const overlayImg = overlay.querySelector('img');
   overlayImg.src = img.src;
   overlay.classList.add('active');
 }
 
-function closeFullscreen() {
+function closeFullscreen() 
+{
   const overlay = document.getElementById('fullscreen-overlay');
   overlay.classList.remove('active');
 }
